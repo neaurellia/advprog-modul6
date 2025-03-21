ebfb16f309e634023813d59a42f13f42560ff793
neaurellia <catherineaurellia@gmail.com>
2025-03-21 08:07:40 +0700

    (2) Returning HTML

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
-
+// --snip--
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-
         handle_connection(stream);
     }
 }
@@ -25,5 +23,14 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
 
 
-    println!("Request: {:#?}", http_request);
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
